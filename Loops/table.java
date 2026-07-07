e13af4a2a1bdc9850d84f92e2ee2326ae4c16ebf
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-07-07 14:05:57 +0530

    normal table loop

---
+++ Loops/table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class table{
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner (System.in);
+        int A = sc.nextInt();
+
+        for (int i = 1 ; i <= 10 ; i ++){
+            int table = A*i; 
+            System.out.println(table);
+        }
+    }
+}
