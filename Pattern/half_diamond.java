33293958ed6a606170d66219d04fd304ce3ba26e
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-07-17 14:23:32 +0530

    half diamond

---
+++ Pattern/half_diamond.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class half_diamond{
+    public static void main (String[] args){
+
+        System.out.println("Enter Grid Size");
+        Scanner sc = new Scanner (System.in);
+        int k = sc.nextInt();
+        System.out.println("Here is your pattern");
+
+        for (int i = 0 ; i < k ; i++){
+            
+            for( int j = 0 ; j <= i ; j++ ){
+                System.out.print("*"); 
+                
+            }
+            System.out.println(" "); // line chang krne ke liye 
+        }
+        for (int m = 0 ; m < k ; m++){
+
+            for (int n = k ; n > m+1 ; n--){
+                System.out.print("*");
+            }
+
+            System.out.println(" "); // line chang krne ke liye 
+        }
+    }
+}
+
+// output for grid size 5
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
