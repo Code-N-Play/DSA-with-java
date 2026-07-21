d6cecda63a47fc83db1f33e7894c0017e5c31279
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-07-21 13:48:03 +0530

    Update perfect_num.java

--- Number Logic/perfect_num.java
+++ Number Logic/perfect_num.java
@@ -1,3 +1,9 @@
+// what is perfect Number :- 
+// a perfect number is a positive integer that is exactly equal to the sum of its proper positive divisors (excluding the number itself)
+
+// example :- 6: The proper divisors of (6) are (1, 2, 3).
+// Sum: (1 + 2 + 3 = 6)
+
 import java.util.Scanner;
 
 public class perfect_num{
