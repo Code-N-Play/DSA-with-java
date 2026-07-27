76d411d920cfaa925394e184f511a47a0686b2f2
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-07-27 14:20:47 +0530

    finding Smallest differance in array

---
+++ Array/SmallestDifferance.java
@@ -0,0 +1,24 @@
+public class SmallestDifferance{
+    public static void main (String [ ] args){
+        
+        int[] array = {111,2,3,45,6,7,8};
+        int array_len = array.length;
+        int temp = 0 ;
+        for(int j = 0 ; j < array_len ; j++){
+            for (int i = 0 ; i < array_len -1 ; i++){
+                if (array[i] > array[i+1]){
+                    temp = array[i+1];
+                    array[i+1] = array[i];
+                    array[i] = temp;
+                    
+                }
+            }
+
+        }
+
+        int Smallest_Differance = array[1] - array[0];
+
+        System.out.println("Smallest Differance in Array is : " +Smallest_Differance); 
+
+    }
+}
