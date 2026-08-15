977ca3835d7d2fa1236d4785e15e71c406174929
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-08-15 15:14:17 +0530

    Find the SubArray whose Sum is equal to given inpt
    Refactor code to improve readability and structure.

--- Array/FIndSubArrayBySum.java
+++ Array/FIndSubArrayBySum.java
@@ -1,42 +1,34 @@
 import java.util.Scanner;
 
-public class FIndSubArrayBySum{
-    public static void main(String[] args){
+public class FIndSubArrayBySum {
+    public static void main (String[] args){
 
         int[] array = {1,2,3,4};
         int array_len = array.length;
         int sum = 0 ; 
+        int starting = 0 ;
+        int ending = 0 ;
 
-        System.out.println("Enter the sub narray sum which you want to find : ");
+        System.out.println("Enter the Sum ....");
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        int index = 0;
 
         for(int i = 0 ; i < array_len ; i++){
-            for(int j = i ; j < array_len ; j++){
-                sum = 0;
-                for(int k = i ; k <= j ; k++ ){
+            for(int j = i ; j<array_len ; j++){
+                sum = 0 ;
+                for(int k = i ; k <= j ; k++){
                     sum = sum + array[k];
-                    if (sum == input){
-                        index = i;
-                    }
+                    
+                }
+                if (sum == input){
+                    starting = i;
+                    ending = j ;
                 }
             }
         }
 
-         for(int i = 0 ; i < array_len ; i++){
-            for(int j = i ; j < array_len ; j++){
-                for(int k = i ; k <= j ; k++ ){
-                    if (i == index){
-                        System.out.print( array[k] + ", ");
-                    }
-                }
-            }
+        for(int i = starting ; i<= ending ; i++){
+            System.out.print(array[i] + ", ");
         }
-        
-
     }
 }
-        
-
-  
